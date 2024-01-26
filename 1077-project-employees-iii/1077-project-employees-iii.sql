5b4746e3f9b2b2e8c1009880f0fa83a234325e48
annie2000 <ihnaee@gmail.com>
2024-01-25 16:25:29 -0800

    Time: 1292 ms (26.94%), Space: 0B (100.00%) - LeetHub

---
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+
+with t as (
+    select p.project_id, p.employee_id, e.name, e.experience_years,
+    Dense_rank() over(partition by p.project_id order by e.experience_years desc) rnk
+    from Project p
+    left join Employee e
+    on p.employee_id = e.employee_id
+    
+)
+
+select project_id, employee_id
+from t
+where rnk =1
