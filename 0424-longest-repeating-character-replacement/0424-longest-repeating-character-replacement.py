dc40127669400889fa8505b14640fc92d7a64545
annie2000 <ihnaee@gmail.com>
2024-02-07 07:22:28 -0800

    Time: 88 ms (70.27%), Space: 16.7 MB (58.23%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,12 +5,13 @@ class Solution:
         res = 0
         
         l = 0
-        # maxf = 0
+        maxf = 0
         
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
             
-            while(r - l +1) - max(count.values()) >k:
+            while(r - l +1) - maxf >k:
                 count[s[l]] -= 1
                 l += 1
             
