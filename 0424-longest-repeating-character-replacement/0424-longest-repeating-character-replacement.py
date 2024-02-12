f4a62499ca64c825a3801f21f940be12dc8e2ae3
annie2000 <ihnaee@gmail.com>
2024-02-12 07:03:33 -0800

    Time: 127 ms (30.21%), Space: 16.7 MB (71.70%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,11 +5,12 @@ class Solution:
         cnt = {}
         
         for r in range(len(s)):
-            cnt[s[r]] = cnt.get(s[r], 0) +1
+            cnt[s[r]] = cnt.get(s[r], 0) + 1
             
-            while r-l +1 -max(cnt.values()) > k:
+            while r - l + 1 - max(cnt.values()) > k:
                 cnt[s[l]] -= 1
-                l += 1
-            res = max(res, (r-l +1))
+                l +=1
+                          
+            res = max(res, (r-l+1)) 
+        
         return res
-               
