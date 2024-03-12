f970ae8e77c9af706d48fbda854edcde90c4920e
annie2000 <ihnaee@gmail.com>
2024-03-12 14:24:05 -0700

    Time: 986 ms (10.23%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,25 +1,9 @@
-# Write your MySQL query statement beloww
+# Write your MySQL query statement below
 
-
-# with t as (
-#     select e2.name as name
-#         ,count(*) ct
-#     from employee e1
-#     left join employee e2
-#     on e2.id = e1.managerId
-#     group by e1.managerId
-#     having ct>=5
-# )
-# select Name
-# from t
-# where name is not null
-
-with mgr as(
-    select distinct(managerId) mgr, count(id) over(partition by managerId) ct
+with cte as (
+    select id, name, managerId, count(id) over(partition by managerId) ct
     from Employee
-    )
-select name
-from Employee 
-where id in (select mgr from mgr where ct>=5 )
-
-    
+)
+select name 
+from employee
+where id in (select managerId from cte where ct>=5)
