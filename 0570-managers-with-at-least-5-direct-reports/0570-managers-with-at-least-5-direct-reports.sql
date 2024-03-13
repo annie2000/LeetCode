aff4f4170d6974b3d769e7c2578d1c52b55fad9b
annie2000 <ihnaee@gmail.com>
2024-03-12 18:58:00 -0700

    Time: 725 ms (33.88%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 
 with cte as (
-    select id, name, managerId, count(id) over(partition by managerId) ct
-    from Employee
+    select *, count(id) over(partition by managerId) ct
+    from employee
 )
-select name 
+select name
 from employee
 where id in (select managerId from cte where ct>=5)
