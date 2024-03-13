975391e5d43499542b17cec8080141a9a4700a65
annie2000 <ihnaee@gmail.com>
2024-03-13 07:48:01 -0700

    Time: 45 ms (61.47%), Space: 17.2 MB (19.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,37 +1,62 @@
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         row, col = len(matrix), len(matrix[0])
+        
+#         topR, botR = 0, row-1
+#         # midR = (topR + botR)//2
+        
+#         while topR <=botR:
+#             midR = (topR + botR)//2
+#             if target < matrix[midR][0]:
+#                 botR = midR -1
+#             elif target > matrix[midR][-1]:
+#                 top = midR + 1
+#             else:
+#                 break
+        
+#         if not(topR<=botR):
+#             return False
+        
+#         l, r = 0, col-1
+#         midR = (topR + botR)//2
+        
+#         while l <= r:
+#             mid = (l+r)//2
+#             if target < matrix[midR][mid]:
+#                 r = mid -1
+#             elif target > matrix[midR][mid]:
+#                 l = mid + 1
+#             else:
+#                 return True
+#         return False
+                
+            
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         row, col = len(matrix), len(matrix[0])
-        topR = 0
-        botR = len(matrix) -1
-        row = (topR + botR)//2
         
-        while topR <=botR:
-            row = (topR + botR)//2
+        topR, botR = 0, row-1
         
-            if target < matrix[row][0]:
-                botR = row -1
-            elif target > matrix[row][-1]:
-                topR = row + 1
+        while topR <= botR:
+            midR = (topR + botR) // 2
+            if target < matrix[midR][0]:
+                botR = midR - 1
+            elif target > matrix[midR][col-1]:
+                topR = midR + 1
             else:
                 break
-                
-        if not(topR <=botR):
+        
+        if not (topR <= botR):
             return False
         
-        l = 0
-        r = col-1
+        l, r = 0, col-1
+        
         while l <= r:
-            mid = (l + r)//2
-            if target < matrix[row][mid]:
-                r = mid -1
-            elif target > matrix[row][mid]:
+            mid = (l + r) // 2
+            if target < matrix[midR][mid]:
+                r = mid - 1
+            elif target > matrix[midR][mid]:
                 l = mid + 1
             else:
                 return True
         return False
-            
-        
-        
-        
-        
-        
