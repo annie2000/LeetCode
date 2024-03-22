1f6f5e5dea8d76dba030347b0470405bf6825df4
annie2000 <ihnaee@gmail.com>
2024-03-22 07:26:50 -0700

    Time: 95 ms (35.09%), Space: 20.3 MB (42.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,41 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mydic = {}
+        mydic = collections.defaultdict(list)
         
-       
         for word in strs:
-            sorW = ''.join(sorted(word))
-            if sorW not in mydic:
-                mydic[sorW] = []
-            mydic[sorW].append(word)
-        res = list(mydic.values())
-        return res
-
-    
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-#         mydic = collections.defaultdict(list)
-#         #{('a', 'e', 't'): ['eat', 'tea', 'ate'], ('a', 'n', 't'): ['tan', 'nat'], ('a', 'b', 't'): ['bat']})
+            key = ''.join(sorted(word))
+            mydic[key].append(word)
         
-        
-
-#         for word in strs:
-#             sor_word = ''.join(sorted(word)) 
-#             mydic[sor_word].append(word)
-        
-#         print(mydic)
-#         return mydic.values()
-            
-        
-
-            
-        
-        
-        
-    
-            
-            
-            
-            
+        return mydic.values()
         
