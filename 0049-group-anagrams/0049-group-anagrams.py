170d1c269e54a9b8d61b81b9e42321154a6e0632
annie2000 <ihnaee@gmail.com>
2024-03-22 07:03:18 -0700

    Time: 78 ms (94.81%), Space: 19.6 MB (76.81%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mydic = {}
+        res = []
+       
+        for word in strs:
+            sorW = ''.join(sorted(word))
+            if sorW not in mydic:
+                mydic[sorW] = []
+            mydic[sorW].append(word)
+        res = list(mydic.values())
+        return res
+        
+        
+
+            
+        
+        
+        
+    
+            
+            
+            
+            
+        
