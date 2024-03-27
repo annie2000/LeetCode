d8099849eed56788d803a01e492bc562d6e0c8a4
annie2000 <ihnaee@gmail.com>
2024-03-27 07:24:46 -0700

    Time: 701 ms (49.49%), Space: 45.1 MB (5.01%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,14 +1,44 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # sort (make pair (position, speed)) by position 
+        # target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+        # after sorting, sortbyposition = [[0, 1], [3,3],[5, 1], [8, 4], [10, 2]]
+        # distance = target - sportbyposition[0]
+        # for the first element of sortbyposition, 
+        # the distance is 12 and the time to arrive the target -> time = distance/sortbyposition[1]
+        # time will be -> [12, 4, 7, 1, 1]
+        # loop over the time from the end -> [1, 1, 7, 3, 12] -> time[앞에 있는것] >=time [뒤에 있는거]: count +=1
+        # return the count 
         
-        res = [[p,s] for p, s in zip(position, speed)]
+        pair = [[p, s] for p, s in zip(position, speed)]
+        # print(pair)
+        sorP = sorted(pair, key =lambda pair:pair[0], reverse = True)
+        # print(sorP) -> [[10, 2], [8, 4], [5, 1], [3, 3], [0, 1]]
+        
+        distance =[]
+        time =[]
         stack = []
         
-        for p, s in sorted(res)[::-1]:
-            t = (target - p)/s
+        for val in sorP:
+            tempD = target - val[0]
+            distance.append(tempD)
+        # print(distance) [2, 4, 7, 9, 12]
+            tempT = tempD/val[1]
+            time.append(tempT)
+        #print(time) [1.0, 1.0, 7.0, 3.0, 12.0]
+        
+        for t in time:
             stack.append(t)
-            if len(stack) >=2 and stack[-1]<= stack[-2]:
+            if len(stack) >=2 and stack[-1] <= stack[-2]:
                 stack.pop()
+
+        
         return len(stack)
-                
+            
+            
+            
+        
+        
+        #sorP = sorted(pair, key=lambda x: x[0])
+        
         
